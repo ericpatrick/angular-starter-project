0a9e950351d9be98198a8d88cea56839a6f28498
Eric Patrick <ericpatrick15@gmail.com>
2021-09-09 19:04:15 -0300

    Correção das rotas

--- src/app/main/main.routing.ts
+++ src/app/main/main.routing.ts
@@ -5,4 +5,4 @@ import { MainComponent } from "./main.component";
 
 const routes: Routes = [{ path: "", component: MainComponent }];
 
-export const routing: ModuleWithProviders = RouterModule.forChild(routes);
+export const routing: ModuleWithProviders<RouterModule> = RouterModule.forChild(routes);
